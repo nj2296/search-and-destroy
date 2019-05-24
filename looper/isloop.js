26c3158aeb83bef9f10fc162a8918df84c40c08a
Nida Jabbar <nj2296@columbia.edu>
2019-05-24 12:43:16 -0400

    Looper test specs passing with extra credit

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,33 @@
 'use strict';
 
 //Complete this algo
-const isLoop = (linkedlist) => {
-
+const isLoop = linkedlist => {
+  let currentNode = linkedlist.head;
+  let values = {};
+  while (currentNode !== null) {
+    if (currentNode.value in values) {
+      return true;
+    } else {
+      values[currentNode.value] = true;
+    }
+    currentNode = currentNode.next;
+  }
+  return false;
 };
 
+const findLoop = linkedlist => {
+  let currentNode = linkedlist.head;
+  let values = {};
+  while (currentNode !== null) {
+    if (currentNode.value in values) {
+      return currentNode.value;
+    } else {
+      values[currentNode.value] = true;
+    }
+    currentNode = currentNode.next;
+  }
+  return false;
+};
 
 /*
 EXTRA CREDIT:
@@ -14,4 +37,7 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = {
+  isLoop,
+  findLoop,
+};
