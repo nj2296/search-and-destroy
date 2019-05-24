81cecebd323f30458c788dfc3a72c69ed9c128ae
Cara Takemoto <cktakemoto@gmail.com>
2019-05-24 12:15:58 -0400

    passed binary searc specs and extra credit

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,20 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
-};
+// const binarySearch = (array, target) => {
+//   if (array.length < 1 || (array.length === 1 && target !== array[0])) {
+//     return false;
+//   }
+//   let middleInd = Math.floor(array.length / 2);
+
+//   if (array[middleInd] === target) {
+//     return true;
+//   } else if (target < array[middleInd]) {
+//     return binarySearch(array.slice(0, middleInd), target);
+//   } else {
+//     return binarySearch(array.slice(middleInd), target);
+//   }
+// };
 
 /*
 	EXTRA CREDIT:
@@ -14,4 +25,23 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+const binarySearch = (array, target, left = 0, right = array.length - 1) => {
+  let arrLength = right - left + 1;
+
+  console.log('array: ', array.slice(left, right + 1));
+
+  if (arrLength < 1 || (arrLength === 1 && target !== array[0])) {
+    return false;
+  }
+  let middleInd = left + Math.floor(arrLength / 2);
+
+  if (array[middleInd] === target) {
+    return true;
+  } else if (target < array[middleInd]) {
+    return binarySearch(array, target, left, middleInd - 1);
+  } else {
+    return binarySearch(array, target, middleInd, right);
+  }
+};
+
+module.exports = binarySearch;
